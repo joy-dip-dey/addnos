c9aa0eba0cc1ecd57fdd5674c42f59ac600d89cf
Joydip Dey <joydipd@nihilentanalytics.com>
2019-08-23 18:40:17 +0530

    addnos5

--- actions/adtwonos.py
+++ actions/adtwonos.py
@@ -4,7 +4,13 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, inp1, inp2):
-        a = max(inp1,inp2)
+        if (inp1 > inp2): 
+            print(inp1+" is greater "+inp2)
+        else:
+            print(inp2+" is greater "+inp1)  
+        #a = max(inp1,inp2)
+        #b = min(inp1, inp2)
+        #c = a 
         # a = int(inp1) + int(inp2)
-        print(a)
-        return(True,a)
+        #print(a)
+        return(True)
