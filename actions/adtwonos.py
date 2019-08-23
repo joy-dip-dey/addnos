d2ada3b65ccbe77ccd97a1c850006e1ce3702f3a
Joydip Dey <joydipd@nihilentanalytics.com>
2019-08-23 18:06:13 +0530

    addnos3

--- actions/adtwonos.py
+++ actions/adtwonos.py
@@ -4,6 +4,7 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, inp1, inp2):
-        a = int(inp1) + int(inp2)
+        a = max(inp1,inp2)
+        # a = int(inp1) + int(inp2)
         print(a)
         return(True,a)
