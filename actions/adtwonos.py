30b02f6deb1cd43922c1540ce5e0da80d180118b
Joydip Dey <joydipd@nihilentanalytics.com>
2019-08-23 18:53:23 +0530

    addnos12

--- actions/adtwonos.py
+++ actions/adtwonos.py
@@ -4,7 +4,7 @@ from st2common.runners.base_action import Action
 class MyAction(Action):
 
     def run(self, inp1, inp2):
-        if (inp1 > inp2): 
+        if (int(inp1) > int(inp2): 
             print(inp1+" is greater "+inp2)
         else:
             print(inp2+" is greater "+inp1)  
