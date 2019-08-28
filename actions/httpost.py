bed1d00ad6ad7f6cdb77740c3b45ef5d369922b9
Joydip Dey <joydipd@nihilentanalytics.com>
2019-08-28 17:33:59 +0530

    httpost-2

--- actions/httpost.py
+++ actions/httpost.py
@@ -5,21 +5,24 @@ import json
 
 class MyAction(Action):
 
-    def run(self, inp1, inp2, inp3, inp4, inp5, inp6):
+    def run(self, inp1, inp2, inp3, inp4, inp5):
         URL = "https://fakerestapi.azurewebsites.net/api/Books"
+        today = datetime.now()
+        nowintimestmp = datetime.timestamp(today)
         inpdata = {
                    "ID": inp1,
                    "Title": inp2,
                    "Description": inp3,
                    "PageCount": inp4,
                    "Excerpt": inp5,
-                   "PublishDate": inp6
+                   "PublishDate": str(today)
                   }
 		  
         header = {
 		          'Content-Type': 'application/json',
 		          'Accept': 'application/json'
                  }
+
         DATA = json.dumps(inpdata)		  
         r = requests.post(url = URL, data = DATA, headers = header)
         output = r.json()
